9b778090cb196691ea4b7991ef89551c358d1318
conniehtli <connie1488>
2018-04-03 23:07:57 -0400

    .

--- Parameters.py
+++ Parameters.py
@@ -5,7 +5,7 @@ UNFAIR_PROB_HEAD = 0.45
 ALPHA = 0.05        # significance level
 
 # settings for steady-state simulation
-SIM_SET_SIZE = 1000     # number of games of the simulated game set (each cohort would have 1000 games)
+SIM_SET_SIZE = 1000     # number of games of the simulated game set
 
 # settings for transient-state simulation
 REAL_SET_SIZE = 10      # size of the real number of games to make the projections for
